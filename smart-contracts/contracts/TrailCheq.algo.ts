bb1c34d3ec477895b0f5cdecedf09fc29ef8dc89
Aspirin <samuellyworld@gmail.com>
2024-10-31 19:35:40 +0100

    update smart contract

--- smart-contracts/contracts/TrailCheq.algo.ts
+++ smart-contracts/contracts/TrailCheq.algo.ts
@@ -1,17 +1,25 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
 
+interface ProductInfo {
+      name: string;
+      description: string;
+      imageURL: string;
+      owner: string;
+      checked: boolean;
+}
+
 
 export class TrailCheq extends Contract {
    // Counter for total products
    productCount = GlobalStateKey<uint64>({ key: 'count' });
     
    // Box storage for multiple products
-   products = BoxMap<uint64, string>({ prefix: 'product' }); 
+   products = BoxMap<uint64, ProductInfo>({ prefix: 'product' }); 
    productNFTs = BoxMap<uint64, AssetID>({ prefix: 'nft' });
    
 
-   createProduct(id: uint64, name: string, description: string, imageURL: string): void {
+   createProduct(id: uint64, name: string, description: string, imageURL: string, owner: string): void {
        // Verify product doesn't exist
        assert(!this.products(id).exists);
         
@@ -25,7 +33,14 @@ export class TrailCheq extends Contract {
        });
        
        // Store product info
-       const productInfo = name + "|" + description + "|" + imageURL;
+      //  const productInfo = name + "|" + description + "|" + imageURL + "|" + owner;
+            const productInfo : ProductInfo = {
+                  name: name,
+                  description: description,
+                  imageURL: imageURL,
+                  owner: owner,
+                  checked: false
+            };
        this.products(id).value = productInfo;
        this.productNFTs(id).value = nftId;
        
@@ -33,9 +48,13 @@ export class TrailCheq extends Contract {
        this.productCount.value = this.productCount.value + 1;
    }
 
-   getProduct(id: uint64): [string, AssetID] {
+   getProduct(id: uint64): [ProductInfo, AssetID] {
        // Verify product exists
        assert(this.products(id).exists);
+       // if it has been checked before, assert
+      assert(!this.products(id).value.checked);
+      // mark as checked
+      this.products(id).value.checked = true;
        return [
             this.products(id).value,
             this.productNFTs(id).value
@@ -47,7 +66,14 @@ export class TrailCheq extends Contract {
        assert(this.products(id).exists);
        
        // Update product info
-       const productInfo = name + "|" + description;
+      //  const productInfo = name + "|" + description;
+            const productInfo : ProductInfo = {
+                  name: name,
+                  description: description,
+                  imageURL: this.products(id).value.imageURL,
+                  owner: this.products(id).value.owner,
+                  checked: this.products(id).value.checked
+            };
        this.products(id).value = productInfo;
    }
 
